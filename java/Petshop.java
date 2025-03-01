57ca794bdd1904fb39b834642801b72c6473c917
nenematoda <natashadind@upi.edu>
2025-03-01 22:38:35 +0700

    java init

--- java/Petshop.java
+++ java/Petshop.java
@@ -1,4 +1,3 @@
-// import java.util.ArrayList; //mengimpor arraylist (supaya bisa dinamis) 
 import java.util.List; //interface dalam java supaya memudahkan penggunaan list (arraylist, linkedlist, vector)
 import java.util.Iterator; //interface dalam java supaya memudahkan penggunaan list (arraylist, linkedlist, vector)
 
@@ -29,75 +28,5 @@ public class Petshop {
     
     public int get_harga(){return harga;}
     public void set_harga(int price){ this.harga = price;}
-
-    // method untuk melakukan print
-    public final void print(){ // menggunakan non-access modifier final supaya variabel yang akan diakses tidak dapat dioverride secara sengaja atau tidak sengaja
-        System.out.println(ID + ". " + produk + " " + kategori + " Rp" + harga); //print dengan concantentation
-    }
-
-    //CRUD untuk class petshop
-    // seluruh method dibuat static karena hanya mengakses objek (kelas) pada list saja, bukan atribut dalam suatu kelas secara spesifik (mengosongkan kelas)
-    public static void addAttributes(List<Petshop> petshops, String produk, String kategori, int harga){
-        Petshop newAttribute = new Petshop(nextID++, produk, kategori, harga); //instansiasi objek
-        petshops.add(newAttribute); // menambahkan objek ke list       
-    }
-
-    public static void displayAttributes(List<Petshop> petshops){
-        if(petshops.isEmpty()){
-            System.out.println("Petshop butuh supply :,(");
-            return;
-        }
-
-        System.out.println("=============================");
-        System.out.println("        PETSHOP DPBO         ");
-        System.out.println("=============================");
-
-        for (Petshop p:petshops){ // print dengan menggunakan enhanced forloop, dimana kita melakukan instansiasi class Petshop dengan nama p dan setiap iterasi program akan membaca isi dari list dan memasukannya kedalam p lalu di print
-            p.print();
-        }
-    }
-
-    public static void updateAttribute(List<Petshop> petshops, int id, String newProduk, String newKategori, int newHarga){
-        boolean updated = false;
-        for(Petshop p:petshops){
-            if(p.get_ID() == id){
-                if(!newProduk.equals("-")){
-                    p.set_produk(newProduk);
-                    updated = true;
-                }   
-                if(!newKategori.equals("-")){
-                    p.set_kategori(newKategori);
-                    updated = true;
-                }   
-                if(newHarga >= 0){
-                    p.set_harga(newHarga);
-                    updated = true;
-                }
-            }
-        }
-        if(updated == true){
-            System.out.println("Atribut berhasil diupdate.");
-            displayAttributes(petshops);
-        } 
-        else System.out.println("Atribut tidak ditemukan.");
-    }
-
-    public static void deleteAttribute(List<Petshop> petshops, int id){
-        Iterator<Petshop> iterator = petshops.iterator(); // looping menggunakan iterator supaya loop lebih efisien dan aman
-        int flag = 0;
-
-        while (iterator.hasNext() && flag == 0) {
-            Petshop p = iterator.next();
-            if (p.get_ID() == id) {
-                iterator.remove(); // hapus elemen dari list
-                flag = 1;
-                System.out.println("Atribut berhasil dihapus dari list.");
-                return; 
-            }
-        }
-        
-        System.out.println("Atribut dengan id" + id + "tidak ditemukan.");
-        displayAttributes(petshops); // tampilkan atribut yang tersedia
-    }
 
 }
